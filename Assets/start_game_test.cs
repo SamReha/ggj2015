a110c33e8e474e2884a82929eb301714381e532e
SamReha <sreha@ucsc.edu>
2015-01-24 11:42:26 -0800

    Implemented test button to load level test

---
+++ Assets/start_game_test.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class start_game_test : MonoBehaviour {
+
+	// Update is called once per frame
+	public void start_game () {
+		Application.LoadLevel ("test");
+	}
+}
---
+++ Assets/test.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+	
+	// Update is called once per frame
+	public void start_game () {
+		Application.LoadLevel ("test");
+	}
+}
